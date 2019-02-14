ceec501cc19889401614740f19c1dc59aa6f539d
Ryan Glascock <rmggz4@mail.missouri.edu>
2019-02-14 17:28:24 -0600

    Added Interface

---
+++ PersonUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonUI
+//
+//  Created by Ryan Glascock on 2/14/19.
+//  Copyright © 2019 Ryan Glascock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
